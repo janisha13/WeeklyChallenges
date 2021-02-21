6139afab8f1413d77414a91cebc6e3fb532f57d7
janisha <65414656+janisha13@users.noreply.github.com>
2021-02-21 08:31:07 -0600

    update challenge1

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -18,32 +18,54 @@ namespace ChallengesWithTestsMark8
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            return minuend - subtrahend;
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            return number1 += number2;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            //if(number1 < number2)
+            //{
+            //    return number1;
+            //}
+            //return number2;
+
+            return number1 < number2 ? number1 : number2;
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            //var answer = factor1 * factor2;
+            //return answer;
+
+            return factor1 * factor2;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            //Console.Write("What is your name?");
+            //var name = Console.ReadLine();
+            //GetGreeting(name);
+
+            if(nameOfPerson != "")
+            {
+                return ($"Hello, {nameOfPerson}");
+            }
+            else
+            {
+                return "Hello!";
+            }
+
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            return "HEY!";
+
         }
     }
 }
